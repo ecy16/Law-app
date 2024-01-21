564dc39371b27d3b71a7e72dc6b53083a74a32a9
ecy16 <ecylamichaels16@gmail.com>
2024-01-21 20:40:41 +0300

    LawFirm-APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent, RouterLink, RouterModule, FooterComponent]
 })
 export class AppComponent {
   title = 'Law-app';
+
 }
