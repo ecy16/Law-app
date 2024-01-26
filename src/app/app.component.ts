676d163bd87e72438de838a052d56cab3dfea997
ecy16 <ecylamichaels16@gmail.com>
2024-01-26 11:01:30 +0300

    Added  the footer buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RouterLink } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from "./footer/footer.component";
+import { FooterComponent } from './footer/footer.component';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HomeComponent, RouterLink, RouterModule, FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    RouterLink,
+    RouterModule,
+    FooterComponent,
+    MatIconModule
+  ],
 })
 export class AppComponent {
   title = 'Law-app';
-
+ 
 }
--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-practice',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './practice.component.html',
   styleUrl: './practice.component.scss'
 })
