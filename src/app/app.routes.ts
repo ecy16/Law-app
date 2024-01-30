f5f267be9bc94b22b3d7fca6295b547ff0157bbe
ecy16 <ecylamichaels16@gmail.com>
2024-01-30 20:19:21 +0300

    Added the People page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { PracticeComponent } from './practice/practice.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { LayoutComponent } from './layout/layout.component';
+import { PeopleComponent } from './people/people.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent,pathMatch:'full'},
@@ -12,6 +13,8 @@ export const routes: Routes = [
     {path:'about',component:AboutComponent},
     {path:'practice',component:PracticeComponent},
     {path:'contacts',component:ContactsComponent},
+    {path:'people',component:PeopleComponent},
+
 
 
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,68 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss',
-    imports: [HeaderComponent,RouterOutlet,FooterComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    FooterComponent,
+    SlickCarouselModule,
+  ],
 })
 export class HomeComponent {
-constructor(private router:Router){}
-ngOnInit():void{}
-onButtonClick(){
-  this.router.navigate(['/about'])
-}
-onBtnClick(){
-  this.router.navigate(['/practice'])
-}
+  constructor(private router: Router) {}
+  ngOnInit(): void {}
+  onButtonClick() {
+    this.router.navigate(['/about']);
+  }
+  onBtnClick() {
+    this.router.navigate(['/practice']);
+  }
+  slides = [
+    { img: './assets/lawGavel.jpg' },
+    { img: './assets/lawPartner.jpg' },
+    { img: './assets/lawPartner2.jpg' },
+    { img: './assets/booksBanner.jpg' },
+  ];
+  slideConfig = {
+    slidesToShow: 2,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplayspeed: 4000,
+    infinite: true,
+    responsive:[
+      {
+        breakpoint:992,
+        settings:{
+          arrows:true,
+          infinite:true,
+          slidesToShow:2,
+          slidesToScroll:2
+        }
+      }
+    ]
+  };
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 
 }
